83a28a7ef61fe175d772c4d08b07e4fddf3f4474
John Arboleda <JohnJack0126@gmail.com>
2021-06-30 11:19:03 -0500

    Update file names for Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
   include UsersHelper
 
   def index
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,4 @@
-module UserHelper
+module UsersHelper
   def user_params
     params.require(:user).permit(:username)
   end

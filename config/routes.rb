c116b168d516bbd47770d25ce7b0e636cf8531f4
John Arboleda <JohnJack0126@gmail.com>
2021-07-01 10:16:19 -0500

    Create subscriptions controller and route resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   
-  root 'users#index'
+  root 'events#index'
 
   get 'signup' => 'users#new', as: 'signup'
   get 'login' => 'sessions#new', as: 'login'
   get 'logout' => 'sessions#destroy', as: 'logout'
 
   resources :users
-  resources :sessions
+  resources :sessions, only: [:new, :create, :destroy]
   resources :events
+  resources :subscriptions, only: [:create, :destroy]
 end

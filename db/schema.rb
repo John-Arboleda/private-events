e38af461812c7ce38edc67d49bc9224157b6ac24
John Arboleda <JohnJack0126@gmail.com>
2021-06-30 22:54:43 -0500

    Migrate user_id foreign key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_022525) do
+ActiveRecord::Schema.define(version: 2021_07_01_034212) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_07_01_022525) do
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|

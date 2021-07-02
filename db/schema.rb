509bb525182d3f9a3c5764cc7c8cb0805dcf5cd5
John Arboleda <JohnJack0126@gmail.com>
2021-07-02 16:56:59 -0500

    Fix bug date_select. Change datatype of date model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_01_142706) do
+ActiveRecord::Schema.define(version: 2021_07_02_202219) do
 
   create_table "events", force: :cascade do |t|
     t.string "name"
-    t.string "date"
+    t.date "date"
     t.text "description"
     t.string "location"
     t.datetime "created_at", precision: 6, null: false
